184efec4029af0411caf2df8553963fc3beaf736
dave@arrgyle.com <dave@arrgyle.com>
2013-08-15 13:03:09 -0400

    Wired up the test

--- google/beakers/search_beaker.rb
+++ google/beakers/search_beaker.rb
@@ -1,6 +1,11 @@
 describe "Search", :depth => 'shallow' do
 
-  let(:search) { Formulas::Search.new(@driver) }
+  let(:search) { @formula_lab.mix('search') }
 
+  it 'search returns a relevant result' do
+    search.for 'elemental selenium tips'
+    result = search.result_present? 'Receive a Free, Weekly Tip'
+    result.should be_true
+  end
 
 end
