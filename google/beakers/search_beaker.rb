adf4c266671f1213d796f831360819aa3bb5cca1
dave@arrgyle.com <dave@arrgyle.com>
2013-08-15 12:54:43 -0400

    Generate a formula for Google Search which also generated a matching beaker

---
+++ google/beakers/search_beaker.rb
@@ -0,0 +1,6 @@
+describe "Search", :depth => 'shallow' do
+
+  let(:search) { Formulas::Search.new(@driver) }
+
+
+end
---
+++ google/formulas/search.rb
@@ -0,0 +1,9 @@
+module Formulas
+  class Search < Formula
+
+  # Uncomment out the below to provide a chemist for your formula
+  # include ChemistryKit::Formula::ChemistAware
+
+
+  end
+end
