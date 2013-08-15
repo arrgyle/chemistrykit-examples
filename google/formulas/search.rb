e6911844012fc5f0ec643a0ca564b3ef1eb671b7
dave@arrgyle.com <dave@arrgyle.com>
2013-08-15 13:03:00 -0400

    Added formula actions that leverage the facade layer

--- google/formulas/search.rb
+++ google/formulas/search.rb
@@ -1,9 +1,30 @@
 module Formulas
   class Search < Formula
+    SEARCH_BOX        = { id: 'gbqfq'     }
+    SEARCH_BOX_SUBMIT = { id: 'gbqfb'     }
+    TOP_SEARCH_RESULT = { css: '#rso .g'  }
 
-  # Uncomment out the below to provide a chemist for your formula
-  # include ChemistryKit::Formula::ChemistAware
+    def initialize(driver)
+      super
+      visit
+      verify_page
+    end
 
+    def for(search_term)
+      type SEARCH_BOX, search_term
+      click_on SEARCH_BOX_SUBMIT
+    end
 
-  end
-end
+    def result_present?(search_result)
+      wait_for { displayed?(TOP_SEARCH_RESULT) }
+      text_of(TOP_SEARCH_RESULT).include? search_result
+    end
+
+    private
+
+      def verify_page
+        title.include?('Google').should == true
+      end
+
+  end # Search
+end # Formulas
