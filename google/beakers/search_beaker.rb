641c8472ccb2d736a070420e17cd1bf0f88778d9
dave@arrgyle.com <dave@arrgyle.com>
2013-08-15 13:23:31 -0400

    Added failing example

--- google/beakers/search_beaker.rb
+++ google/beakers/search_beaker.rb
@@ -2,10 +2,19 @@ describe "Search", :depth => 'shallow' do
 
   let(:search) { @formula_lab.mix('search') }
 
-  it 'search returns a relevant result' do
+  before(:each) do
     search.for 'elemental selenium tips'
-    result = search.result_present? 'Receive a Free, Weekly Tip'
-    result.should be_true
+    @result = search.result_present?('Receive a Free, Weekly Tip')
+  end
+
+  it 'search returns a relevant result' do
+    @result.should be_true
+  end
+
+  # This example is meant to fail to show what a failure
+  # looks like in the HTML report in the evidence directory
+  it 'search returns a relevant result redux' do
+    @result.should be_false
   end
 
 end
