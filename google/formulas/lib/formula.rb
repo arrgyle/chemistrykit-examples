d298e30ab315d32344e2aacbe6a7fb96a4aab717
dave@arrgyle.com <dave@arrgyle.com>
2013-08-15 13:02:47 -0400

    Added facade layer actions

--- google/formulas/lib/formula.rb
+++ google/formulas/lib/formula.rb
@@ -1,7 +1,54 @@
 # Encoding: utf-8
 
-# this is where you can define generic helper functions that are inhereted by
-# the rest of your formulas
+# This is where you can define generic helper functions
+# that are inhereted by your formulas.
+# The ones below are borrowed from:
+# http://elemental-selenium.com/tips/9-use-a-base-page-object
+
 class Formula < ChemistryKit::Formula::Base
 
+  attr_reader :driver
+
+  def initialize(driver)
+    @driver = driver
+  end
+
+  def visit(url='/')
+    driver.get(ENV['base_url'] + url)
+  end
+
+  def find(locator)
+    driver.find_element locator
+  end
+
+  def clear(locator)
+    find(locator).clear
+  end
+
+  def type(locator, input)
+    find(locator).send_keys input
+  end
+
+  def click_on(locator)
+    find(locator).click
+  end
+
+  def displayed?(locator)
+    driver.find_element(locator).displayed?
+    true
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      false
+  end
+
+  def text_of(locator)
+    find(locator).text
+  end
+
+  def title
+    driver.title
+  end
+
+  def wait_for(seconds=5)
+    Selenium::WebDriver::Wait.new(:timeout => seconds).until { yield }
+  end
 end
